75a8cdbd7ce736f99f78f2fe8b1fa14d2c054280
Boris <feofart@gmail.com>
2022-07-29 18:56:56 +0300

    GJ

---
+++ src/Lesson2/ZDAROVA.java
@@ -0,0 +1,9 @@
+package Lesson2;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class ZDAROVA {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+}
